01b81b4121ef9efe035ff9f964f73654edf1dc53
Ari625 <arir06101@gmail.com>
2024-01-16 13:30:33 +0700

    1.1

--- membuatcrudmvcjava/src/membuatcrudmvcjava/Model/ModelMember.java
+++ membuatcrudmvcjava/src/membuatcrudmvcjava/Model/ModelMember.java
@@ -11,4 +11,51 @@ package membuatcrudmvcjava.Model;
  */
 public class ModelMember {
     
+    private Integer id;
+    private String nama;
+    private String no_telp;
+    private String alamat;
+    private String paket;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNo_telp() {
+        return no_telp;
+    }
+
+    public void setNo_telp(String no_telp) {
+        this.no_telp = no_telp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+    
+    
 }
--- membuatcrudmvcjava/src/membuatcrudmvcjava/Model/TabelModelMember.java
+++ membuatcrudmvcjava/src/membuatcrudmvcjava/Model/TabelModelMember.java
@@ -5,10 +5,66 @@
  */
 package membuatcrudmvcjava.Model;
 
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Administrator
  */
-public class TabelModelMember {
+public class TabelModelMember extends AbstractTableModel{
+
+    java.util.List<ModelMember> list_member;
+    
+    public TabelModelMember(java.util.List<ModelMember> list_member){
+        this.list_member = list_member;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return list_member.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch(column){
+            case 0:
+                return "ID/Kode";
+            case 1:
+                return "Nama";
+            case 2:
+                return "No Telepon";
+            case 3:
+                return "Alamat";
+            case 4:
+                return "Paket";
+            default:
+                return null;
+        }
+    }
+    
+    @Override
+    public Object getValueAt(int row, int column) {
+        switch(column){
+            case 0:
+                return list_member.get(row).getId();
+            case 1:
+                return list_member.get(row).getNama();
+            case 2:
+                return list_member.get(row).getNo_telp();
+            case 3:
+                return list_member.get(row).getAlamat();
+            case 4:
+                return list_member.get(row).getPaket();
+            default:
+                return null;
+        }
+    }
+    
+    
     
 }
